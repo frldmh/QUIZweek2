5ae8f52cde895b0cb529966936ca8a0515bf2293
Muhammad Hendra Feraldi <54926708+frldmh@users.noreply.github.com>
2023-09-17 22:54:36 +0700

    registration.cy.js

--- registration.cy.js
+++ registration.cy.js
@@ -1,5 +1,26 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('registration', () => {
+// untuk pengujian test case pertama (success registration) selanjutnya akan muncul notif 'the specified email already exists' di web
+
+  it('success registration', () => {
+    cy.visit('https://demowebshop.tricentis.com/')
+    cy.get('.ico-register').click()
+    cy.get('#gender-male').click()
+    cy.get('#FirstName').type('siapa')
+    cy.get('#LastName').type('yak')
+    cy.get('#Email').type('siapayak007@gmail.com')
+    cy.get('#Password').type('siapayak')
+    cy.get('#ConfirmPassword').type('siapayak')
+    cy.get('#register-button').click()
   })
+  it('failed registration', () => {
+    cy.visit('https://demowebshop.tricentis.com/')
+    cy.get('.ico-register').click()
+    cy.get('#gender-male').click()
+    cy.get('#FirstName').type('siapa')
+    cy.get('#LastName').type('yak')
+    cy.get('#Email').type('siapayak007@gmail.com')
+    cy.get('#Password').type('siapayak')
+    cy.get('#ConfirmPassword').type('siap')
+    cy.get('#register-button').click()
+  })  
 })
