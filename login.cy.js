3683b76bf4e599ef062eed403a36f069794c6338
Muhammad Hendra Feraldi <54926708+frldmh@users.noreply.github.com>
2023-09-17 22:55:13 +0700

    Update login.cy.js

--- login.cy.js
+++ login.cy.js
@@ -1,5 +1,24 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('login', () => {
+  it('success login', () => {
+    cy.visit('https://demowebshop.tricentis.com/')
+    cy.get('.ico-login').click()
+    cy.get('#Email').type('siapayak007@gmail.com')
+    cy.get('#Password').type('siapayak')
+    cy.get('form > .buttons > .button-1').click()
+  })
+  it('failed login', () => {
+    cy.visit('https://demowebshop.tricentis.com/')
+    cy.get('.ico-login').click()
+    cy.get('#Email').type('siapayak007@gmail.com')
+    cy.get('#Password').type('hmm')
+    cy.get('form > .buttons > .button-1').click()
+  })
+  it('forgot password when login', () => {
+    cy.visit('https://demowebshop.tricentis.com/')
+    cy.get('.ico-login').click()
+    cy.get('#Email').type('siapayak007@gmail.com')
+    cy.get('.forgot-password > a').click()
+    cy.get('#Email').type('siapayak007@gmail.com')
+    cy.get('form > .buttons > .button-1').click()
   })
 })
